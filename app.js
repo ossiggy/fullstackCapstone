ebc80e6ad6a10749a31d88385047363e9a2aa8f0
Ossiggy <mrossig201@gmail.com>
2017-07-27 00:34:13 -0400

    added updateState function

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
-$("td").on("change", updateHtml)
+$(".changeable").on("blur", updateState)
 
-function updateHtml(event){
-  console.log($(this))
+// will also need a function that takes each change and, if change is made in certain
+// column, add or subtract from monthly income, remaining funds, and total bill
+// amount left over, respectively
+
+function updateState(event){
+  var target = $(event.target);
+  var contents = target.html()
+  if(contents!=$(this).html()){
+    contents = $(this).html()
+  }
+}
+
+function checkChangeLocation(change){
+  if(change.className===)
 }
