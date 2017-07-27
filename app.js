978daeb17ecb44ce46871ef413f3abc2ad4e2f20
Ossiggy <mrossig201@gmail.com>
2017-07-27 02:18:03 -0400

    can now subtract expenses from monthly income

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 $(".changeable").on("blur", updateState)
-
+$("#income").on("blur", rebalance)
 // will also need a function that takes each change and, if change is made in certain
 // column, add or subtract from monthly income, remaining funds, and total bill
 // amount left over, respectively
+
+let amountTotal = 0
 
 function updateState(event){
   var target = $(event.target)
@@ -15,16 +17,26 @@ function updateState(event){
 }
 
 function checkClass(changedContents){
-  var value = changedContents.html()
-  console.log(value)
-  if(changedContents.hasClass("amount")){
-    var income = $("#income").value
+  var value = Number(changedContents.html())
+  var income = Number($("#income").html())
+  if(changedContents.hasClass("amount")&&Number.isInteger(value)){
+    amountTotal = value + amountTotal
+    remainingFunds = income-amountTotal
+    $("#remainingFunds").html(remainingFunds)
   }
-  if(changedContents.hasClass("due-date")){
+  else{
+    console.log("not a number!")
+  }
+  // if(changedContents.hasClass("due-date")){
 
-  }
-  if(changedContents.hasClass("balance")){
+  // }
+  // if(changedContents.hasClass("balance")){
 
-  }
+  // }
 }
 
+function rebalance(event){
+  var income = $(event.target).html()
+  var amount = $(".amount")
+  $("#remainingFunds").html(income-amountTotal)
+}
