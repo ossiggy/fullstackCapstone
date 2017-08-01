60094abc6135f16dd6bcbfd520d8a85405f0cd0f
Ossiggy <mrossig201@gmail.com>
2017-08-01 14:35:55 -0400

    created Budget model

--- assets/models.js
+++ assets/models.js
@@ -1,5 +1,25 @@
 const mongoose = require('mongoose')
 
 const budgetSchema = mongoose.Schema({
-  
+    username: {type: String, required: true},
+    income: {type: Number, required: true},
+    budget: {
+      name: {type: String, required: true},
+      amount: {type: Number},
+      goal: {type: Number},
+      balance: {type: Number}
+    }
 })
+
+budgetSchema.methods.apiRepr = function(){
+  return{
+    id: this._id,
+    username: this.username,
+    income: this.income,
+    budget: this.budget
+  }
+}
+
+const Budget = mongoose.model('Budget', budgetSchema)
+
+module.exports = {Budget}
