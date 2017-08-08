c27d104e76e245017511e08606042438809286be
Ossiggy <mrossig201@gmail.com>
2017-08-08 16:51:03 -0400

    loadState working

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+$(document).ready(loadState)
+
 $('body').on('focus', '[contenteditable]', function() {
     var $this = $(this);
     $this.data('before', $this.html());
@@ -17,8 +19,13 @@ $('body').on('focus', '[contenteditable]', function() {
     }
 });
 
+$('form').on("submit", saveState);
+
 // extract value from user object
 // have a form compile data from our table to submit
+
+let STATE = {}
+let loadedState = {}
 
 function doBalance(amount){
     var amountValue = Number(amount.html())
@@ -38,3 +45,62 @@ function remainingFunds(){
   var remainingFunds = income - totalSpent
   $("#remainingFunds").html(remainingFunds)
 }
+
+function saveState(event) {
+  event.preventDefault()
+// alert("Information Saved!")
+}
+
+
+function loadState(event){
+  // var url = "/budgets/:id"
+  $.getJSON("./seed-data.json", function(response){
+    loadedState = response
+    render(loadedState)
+    remainingFunds()
+	})
+}
+
+// function storeLocally(state) {
+//   loadedState = state
+//   render(loadedState)
+// }
+
+function render(state){
+  $(".income").html(`${state.availableIncome}`)
+    for(var i=0; i<state.categories.length; i++){
+      show(state.categories[i])
+    }
+}
+
+function show(category){
+$(`#${category.table}`).append(
+        `<tr>
+        <th>${category.name}</th>
+        <td contenteditable="true" class="changeable amount">0</td>
+        <td contenteditable="true" class="changeable goal">${category.goal}</td>
+        <td class="balance">${category.balance}</td>
+        </tr>`)
+}
+// WHEN USER SAVES:
+// save categories as new state
+// compare new state to old state
+// compile into one state
+// send new state to server
+// WHEN PAGE LOADS:
+// get budget data from server
+// populate STATE with data
+// use STATE as source for rendering
+// rinse, repeat
+
+// - User loads page 
+// - Page uses jQuery to request budget data from your DB 
+// - jQuery takes the data it gets back and puts it in the state. 
+// We now have a state object that looks like the DB; a sort of local copy of the DB 
+// - User uses the app to make changes to their budget 
+// - jQuery saves these changes in the local state 
+// - When user is done (for now signified by them pressing a button manually) 
+// local state is used to update DB
+
+
+
