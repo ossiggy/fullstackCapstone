eae74eb1552593da17873edddba4e9f70116130b
Ossiggy <mrossig201@gmail.com>
2017-08-12 07:16:38 -0400

    added post endpoint to server.js

--- server.js
+++ server.js
@@ -15,39 +15,11 @@ mongoose.Promise=global.Promise;
 
 app.get('/budgets/:id', (req, res) => {
   Budget
-    .findById(req.params.id);
-    .exec();
-    .then(budget => res.json(budget.apiRepr()));
+    .findById(req.params.id)
+    .exec()
+    .then(budget => res.json(budget.apiRepr()))
     .catch(err => {
       console.error(err);
       res.status(500).json({error: 'something went wrong'})
-    });
-});
-
-
-
-
-
-
-// const STATE = {
-  
-
-// }
-// const form = $('form');
-// const textField = $('#text-field');
-
-// form.on("submit", saveName);
-
-// function saveName(e) {
-//     const userName = textField.val();
-//     STATE.name = userName;
-//     sendToServer(STATE);
-// }
-
-// // WHEN USER SAVES:
-// // send state (or updated relevant parts) to server
-// // WHEN PAGE LOADS:
-// // get budget data from server
-// // populate STATE with data
-// // use STATE as source for rendering
-// // rinse, repeat
+    })
+})
