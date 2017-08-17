d123ea84cf6eb1bb5b6219c958b57db4596ff1e4
Ossiggy <mrossig201@gmail.com>
2017-08-17 00:56:30 -0400

    updated app.js to compile complete user object

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 $(document).ready(loadState)
 $('#pay-day').on('click', payDay)
 $('form').on('click','.pay-it', payBill)
-$('form').on('submit', saveState);
+$('form').on('submit', createState);
 
 // $('#submit').on('click', formSubmit)
 
@@ -19,6 +19,8 @@ function payBill(event){
   let amount = Number($(this).parent().siblings('td[data-column="amount"]').html())
   remainingFunds -= amount
   $('#remainingFunds').html(remainingFunds)
+  this.disabled = true;
+  // $(event.target).css({"background": "#1c57ff"})
 }
 
 function payDay(event){
@@ -26,13 +28,13 @@ function payDay(event){
   let income = Number($('#income').html())
   let remainingFunds = Number($('#remainingFunds').html())
   $('#remainingFunds').html(income+=remainingFunds)
+  this.disabled = true;
 }
 
 function updateState() {
   const savedState = Object.assign({}, newState, loadedState)
   console.log(savedState)
 }
-
 
 function loadState(event){
   // var url = "/budgets/:id"
@@ -47,7 +49,11 @@ function storeLocally(state) {
 }
 
 function render(state){
-  $('.income').html(`${state.weeklyIncome}`)
+  $('#sign-in').append(
+    `<h3 class='welcome'>welcome ${state.username}</h3>
+      <div id='username' class='hidden'>${state.username}</div>`
+  )
+  $('#income').html(`${state.weeklyIncome}`)
   $('#remainingFunds').html(`${state.availableIncome}`)
     for(var i=0; i<state.categories.length; i++){
       show(state.categories[i])
@@ -56,42 +62,36 @@ function render(state){
 
 function show(category){
 $(`#${category.table}`).append(
-        `<tr>
-        <th contenteditable="true" data-table="${category.table}" id="${category.name}">${category.name}</th>
-        <td contenteditable="true" class="changeable amount" data-category="${category.name}" data-column="amount">${category.amount}</td>
-        <td><button type="button" class="pay-it">Paid</button></td>
+        `<tr class="tableData">
+          <th contenteditable="true" class="changeable name" data-table="${category.table}" id="${category.name}">${category.name}</th>
+          <td contenteditable="true" class="changeable amount" data-category="${category.name}" data-column="amount">${category.amount}</td>
+          <td><button type="button" class="pay-it">Paid</button></td>
         </tr>`)
 }
 
-function saveState(event){
+function createState(event){
   event.preventDefault()
-  income = $('.income').html()
-  remainingFunds = $('#remainingFunds').html()
-  newObj = {
+  const username = $('#username').html()
+  const income = $('#income').html()
+  const remainingFunds = $('#remainingFunds').html()
+  const newObj = {
+    username: username,
     weeklyIncome: income,
     availableIncome: remainingFunds,
     categories:[]
   }
-  const categories = $("th[contenteditable]")
-  $.each(categories, function(i, category){
-    let newCategory = category.textContent.toCamelCase()
-    newObj[newCategory] = $(this).siblings('td[contenteditable]').html()
+  const budgets = $('tr.tableData')
+  $.each(budgets, function(i, budget){
+    let newBudget = {
+      table: $(this).closest('table').attr('id'),
+      name: $(this).children('th').html(),
+      amount: $(this).children('td[contenteditable]').html()
+    }
+  newObj.categories.push(newBudget)
   })
-  newState = newObj
+  newState = newObj;
   updateState()
 }
-
-//create a new function that makes each category an object that has name, value, table
-//push those objects to the categories array
-
-// function saveState(){
-//   var fields = $("td[contenteditable]")
-//   $.each(fields, function(i, field) {
-//     let fieldCategory = field.dataset.category.toCamelCase();
-//     newState[fieldCategory] = field.textContent;
-//   })
-//   updateState()
-// }
 
 // function formSubmit(event){
   
