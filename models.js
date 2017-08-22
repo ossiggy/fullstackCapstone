e0dcc477693b5c44ec5e2fd2f76e9fe7eb83dd1c
Ossiggy <mrossig201@gmail.com>
2017-08-22 12:10:31 -0400

    changed models so that it all falls under Budget

---
+++ models.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+
+
+//parent budget
+const budgetSchema = mongoose.Schema({
+   username: {type: String, required: true},
+   availableIncome: {type: Number, required: true},
+   weeklyIncome: {type: Number, required: true},
+   categories: {type: Array, required: true}
+  //  categories: [{type: mongoose.Schema.Types.ObjectId, ref:'Category'}]
+});
+
+// const categorySchema = mongoose.Schema({
+//   _parent:{type: mongoose.Schema.Types.ObjectId, ref:'Budget'},
+//   table: {type:String, required: true},
+//   name: {type: String, required: true},
+//   amount: {type: Number, required: true},
+// })
+
+// one Budget has many BudgetCategories
+// one BudgetCategory has many Properties
+
+budgetSchema.methods.apiRepr = function(){
+  return{
+    id: this._id,
+    username: this.username,
+    availableIncome: this.availableIncome,
+    weeklyIncome: this.weeklyIncome,
+    categories: this.categories
+  }
+}
+
+const Budget = mongoose.model('Budget', budgetSchema)
+// const Category = mongoose.model('Category', categorySchema)
+
+module.exports = {Budget}
