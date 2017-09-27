d1e046f37f2361c94895d79b89a886fb2a5f6f41
Ossiggy <mrossig201@gmail.com>
2017-09-26 20:07:45 -0400

    moved models out of assets and deleted router

--- assets/config.js
+++ assets/config.js
@@ -1,7 +1,9 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
-                      'mongodb://localhost/blog-app';
+                      'mongodb://localhost/budge-my-life';
 exports.TEST_DATABASE_URL = (
   process.env.TEST_DATABASE_URL ||
-  'mongodb://localhost/test-blog-app')
+  'mongodb://localhost/test-budge-my-life')
 exports.PORT = process.env.PORT || 8080;
+exports.JWT_SECRET = process.env.JWT_SECRET;
+exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
