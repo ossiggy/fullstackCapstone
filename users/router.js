b14b29226bbb0797affd38150cd35e2ceb48d870
Ossiggy <mrossig201@gmail.com>
2017-09-27 11:55:25 -0400

    user creation router created

---
+++ users/router.js
@@ -0,0 +1,119 @@
+const {User} = require('./models');
+
+const router = express.Router();
+
+const jsonParser = bodyParser.json();
+
+router.post('/', jsonParser, (req, res) => {
+  const requiredFields = ['username', 'password', 'email'];
+  const missingField = requiredFields.find(field => !(field in req.body));
+
+  if(missingField) {
+    return res.status(422).json({
+      code: 422,
+      reason: 'ValidationError',
+      message: 'Missing field',
+      location: missingField
+    });
+  };
+
+  const stringFields = ['username', 'password', 'email', 'firstName', 'lasName'];
+  const nonStringField = stringFields.find(
+    field => field in req.body && typeof req.body[field] !== 'string'
+  );
+
+  if(nonStringField) {
+    return res.status(422).json({
+      code: 422,
+      reason: 'ValidationError',
+      message: 'Incorrect field type: expected string',
+      location: nonStringField
+    });
+  }
+
+  const explicityTrimmedFields = ['username', 'password', 'email'];
+  const nonTrimmedField = explicityTrimmedFields.find(
+    field => req.body[field].trim() !== req.body[field]
+  );
+
+  if(nonTrimmedField) {
+    return res.status(422).json({
+      code: 422,
+      reason: 'ValidationError',
+      message: 'Cannot start or end with space',
+      location: nonTrimmedField
+    });
+  }
+
+  const sizedFields = {
+    username: {
+      min: 5,
+      max: 15
+    },
+    password: {
+      min: 10,
+      max: 72
+    }
+  };
+  const tooSmallField = Object.keys(sizedFields).find(
+    field =>
+      'min' in sizedFields[field] &&
+      req.body[field].trim().length < sizedFields[field].min
+  );
+  const tooLargeField = Object.keys(sizedFields).find(
+    field =>
+      'max' in sizedFields[field] &&
+      req.body[field].trim().length > sizedFields[field].max
+  );
+
+  if (tooSmallField || tooLargeField){
+    return res.status(422).json({
+      code: 422,
+      reason: 'ValidationError',
+      message: tooSmallField
+        ? `Must be at least ${sizedFields[tooSmallField]
+            .min} characters long`
+        : `Cannot exceed ${sizedFields[tooLargeField]
+            .max} characters`,
+      location: tooSmallField || tooLargeField
+    });
+  }
+
+  let {username, password, email, firstName = '', lastname = ''}
+  firstName = firstName.trim();
+  lastName = lastName.trim();
+
+  return User.find({username})
+    .count()
+    .then(coiunt => {
+      if(count > 0) {
+        return Promise.reject({
+          code: 422,
+          reason: 'ValidationError',
+          message: 'Username unavailable',
+          location: 'username'
+        });
+      }
+
+      return User.hashPassword(password)
+    })
+    .then(hash => {
+      return User.create({
+        username,
+        password: hash,
+        firstName,
+        lastName
+      });
+    })
+    .then(user => {
+      return res.status(201).json(user.apiRepr())
+    })
+    .catch(err => {
+      if(err.reason === 'ValidationError'){
+        return res.status(err.code).json(err)
+      }
+      res.status(500).json({code: 500, message: 'Internal server error'})
+    });
+});
+
+module.exports = {router};
