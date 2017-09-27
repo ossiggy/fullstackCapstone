367f6a7f4012ad98ef88a098128f138c71c862e3
Ossiggy <mrossig201@gmail.com>
2017-09-26 20:46:35 -0400

    split server and budget router fucntionality

--- budgets/budget-router.js
+++ budgets/budget-router.js
@@ -1,23 +1,37 @@
 const express = require('express');
-const router = express.Router();
 const mongoose = require('mongoose')
-const morgan = require('morgan')
-const cors = require('cors')
-const app = express()
 const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
 
+const router = express.Router();
 const {Budget, Category} = require('./models')
 
 mongoose.Promise=global.Promise;
 
-app.get('/budgets/:id', (req, res) => {
+router.get('/budgets', (req, res) => {
+  Budget
+    .find()
+    .populate('categories')
+    .exec(function(err, categories){
+      if(err) return 'error';
+    })
+    .then(budgets => {
+      res.json(budgets.map(budget => budget.apiRepr()))
+    })
+    .catch(err => {
+      console.error(err)
+      res.status(500).json({error: "something went wrong"})
+    })
+})
+
+router.get('/budgets/:id', (req, res) => {
   Budget
     .findById(req.params.id)
     .populate('categories')
-    .exec()
+    .exec(function(err, categories){
+      if(err) return "error";
+    })
     .then(
-     //populate by or either category or budget 
       budget => res.json(budget.apiRepr()))
     .catch(err => {
       console.error(err);
@@ -25,7 +39,7 @@ app.get('/budgets/:id', (req, res) => {
     })
 })
 
-app.post('/budgets', (req, res) => {
+router.post('/budgets', (req, res) => {
   const requiredFields = ['username', 'availableIncome', 'weeklyIncome', 'categories']
   for(let i=0; i<requiredFields.length; i++){
     const field = requiredFields[i]
@@ -36,7 +50,7 @@ app.post('/budgets', (req, res) => {
     }
   }
   const {username, weeklyIncome, availableIncome, categories} = req.body
-  
+
 Budget
   .create({
     username,
@@ -45,7 +59,8 @@ Budget
     categories: []
   })
     .then(
-      budget => {for(let i=0; i<categories.length; i++){
+      budget => {
+        for(let i=0; i<categories.length; i++){
         Category.create({
           _parent: budget._id,
           table: categories[i].table,
@@ -54,29 +69,16 @@ Budget
         })
         .then(
           category => budget.update({$push: {"categories": {_id:category._id}}}, {safe: true, upsert: true})
-        )
-      }
-    budget => res.sendStatus(201)}
-    ).catch(err => {
-      console.error(err)
-      res.status(500).json({message: 'Internal server error'})
+        )}
     })
-    
+    .then(budget => res.status(204).end())
+    .catch(err => {
+        console.error(err)
+        res.status(500).json({message: 'Internal server error'})
+      })   
 })
-  // Budget
-  // //I don’t see you taking the `req.body.categories` data and creating a `Category` out of it. 
-  // //Just because you have told `Budget` that `categories` has an objectId association doesn’t mean it 
-  // //automatically fills in the blanks and creates that for you.
-  // //The first thing I have to do in this example is _find_ the campground, from there I can create a comment 
-  // //and that comment will be associated with the specific campground.
-  //   .create({
-  //     username,
-  //     weeklyIncome,
-  //     availableIncome,
-  //     categories
-  //   })
 
-app.put('budgets/:id', (req, res) => {
+router.put('budgets/:id', (req, res) => {
   if(!(req.params.id && req.body.id && req.params.id)){
     const message = (
       `Request patch id (${req.params.id} and request body id (${req.body.id}) must match)`)
@@ -100,16 +102,12 @@ app.put('budgets/:id', (req, res) => {
     .catch(err => res.status(500).json({message: 'Internal server error'}))
 })
 
-app.delete('/budgets/:id', (req, res) => {
+router.delete('/budgets/:id', (req, res) => {
   Budget
     .findByIdAndRemove(req.params.id)
     .exec()
     .then(post => res.status(204).end())
     .catch(err => res.status(500).json({message: 'Inernal server error'}))
-})
-
-app.use('*', function(req, res){
-  res.status(404).json({message: 'Not found'})
 })
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,123 +3,18 @@ const express = require('express');
 const mongoose = require('mongoose')
 const morgan = require('morgan')
 const cors = require('cors')
+const budgetRouter = require('./budgets/budget-router')
 
 const {DATABASE_URL, PORT} = require("./config")
-const {Budget, Category} = require("./models")
 
 const app = express();
 
 app.use(morgan('common'));
 app.use(bodyParser.json());
 app.use(cors())
+app.use(budgetRouter)
 
 mongoose.Promise=global.Promise;
-
-// app.get('/', (req, res) => {
-//   res.sendFile('./index.html')
-// })
-
-app.get('/budgets', (req, res) => {
-  Budget
-    .find()
-    .populate('categories')
-    .exec(function(err, categories){
-      if(err) return 'error';
-    })
-    .then(budgets => {
-      res.json(budgets.map(budget => budget.apiRepr()))
-    })
-    .catch(err => {
-      console.error(err)
-      res.status(500).json({error: "something went wrong"})
-    })
-})
-
-app.get('/budgets/:id', (req, res) => {
-  Budget
-    .findById(req.params.id)
-    .populate('categories')
-    .exec(function(err, categories){
-      if(err) return "error";
-    })
-    .then(
-      budget => res.json(budget.apiRepr()))
-    .catch(err => {
-      console.error(err);
-      res.status(500).json({error: 'something went wrong'})
-    })
-})
-
-app.post('/budgets', (req, res) => {
-  const requiredFields = ['username', 'availableIncome', 'weeklyIncome', 'categories']
-  for(let i=0; i<requiredFields.length; i++){
-    const field = requiredFields[i]
-    if(!(field in req.body)){
-      const message = `Missing \`${field}\` in request body`
-      console.log(field)
-      console.error(message)
-    }
-  }
-  const {username, weeklyIncome, availableIncome, categories} = req.body
-
-Budget
-  .create({
-    username,
-    weeklyIncome,
-    availableIncome,
-    categories: []
-  })
-    .then(
-      budget => {
-        for(let i=0; i<categories.length; i++){
-        Category.create({
-          _parent: budget._id,
-          table: categories[i].table,
-          name: categories[i].name,
-          amount: categories[i].amount
-        })
-        .then(
-          category => budget.update({$push: {"categories": {_id:category._id}}}, {safe: true, upsert: true})
-        )}
-    })
-    .then(budget => res.status(204).end())
-    .catch(err => {
-        console.error(err)
-        res.status(500).json({message: 'Internal server error'})
-      })   
-})
-
-app.put('budgets/:id', (req, res) => {
-  if(!(req.params.id && req.body.id && req.params.id)){
-    const message = (
-      `Request patch id (${req.params.id} and request body id (${req.body.id}) must match)`)
-      console.error(message)
-      res.status(400).json({message: message})
-  }
-
-  const toUpdate = {}
-  const updateableFields = ['availableIncome', 'weeklyIncome', 'categories.name', 'categories.amount']
-
-  updateableFields.forEach(field => {
-    if (field in req.body) {
-      toUpdate[field] = req.body[field]
-    }
-  })
-
-  Budget
-    .findByIdAndUpdate(req.params.id, {$set: toUpdate})
-    .exec()
-    .then(post => res.status(204).end())
-    .catch(err => res.status(500).json({message: 'Internal server error'}))
-})
-
-app.delete('/budgets/:id', (req, res) => {
-  Budget
-    .findByIdAndRemove(req.params.id)
-    .exec()
-    .then(post => res.status(204).end())
-    .catch(err => res.status(500).json({message: 'Inernal server error'}))
-})
 
 app.use('*', function(req, res){
   res.status(404).json({message: 'Not found'})
