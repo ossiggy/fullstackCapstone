fa47f93dcef62a6282e606c82fcc162ffaeb0a5f
Ossiggy <mrossig201@gmail.com>
2017-10-01 16:39:53 -0400

    sent back user id as part of response

--- auth/authRouter.js
+++ auth/authRouter.js
@@ -19,7 +19,9 @@ router.post(
   passport.authenticate('basic', {session: false}),
   (req, res) => {
     const authToken = createAuthToken(req.user.apiRepr());
-    res.json({authToken});
+    const {id} = req.user.apiRepr();
+    
+    res.json({authToken, id});
   }
 );
 
